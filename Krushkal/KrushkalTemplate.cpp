4647a0012b0e963112a490668aa22dec585a2239
Ajoy Saha <138325582+ajoycodes@users.noreply.github.com>
2025-11-27 10:01:40 +0600

    Updated LIS

--- Krushkal/KrushkalTemplate.cpp
+++ Krushkal/KrushkalTemplate.cpp
@@ -5,41 +5,63 @@
 #include <algorithm>
 using namespace std;
 
-int LIS(vector<int> &a) {
-    vector<int> temp;
-    for (int x : a) {
-        //Non-decreasing hole "upper_bound" use kora lagbe!
-        auto it = lower_bound(temp.begin(), temp.end(), x);
-        if (it == temp.end()) temp.push_back(x);
-        else *it = x;
+using ll = long long;
+
+struct Edge {
+    int u, v;
+    ll w;
+};
+
+struct DSU {
+    vector<int> parent, sz;
+
+    DSU(int n) {
+        parent.resize(n + 1);
+        sz.assign(n + 1, 1);
+        for (int i = 1; i <= n; i++) parent[i] = i;
     }
-    return temp.size();
+
+    int find(int v) {
+        if (parent[v] == v) return v;
+        return parent[v] = find(parent[v]);
+    }
+
+    bool unite(int a, int b) {
+        a = find(a);
+        b = find(b);
+        if (a == b) return false;
+        if (sz[a] < sz[b]) swap(a, b);
+        parent[b] = a;
+        sz[a] += sz[b];
+        return true;
+    }
+};
+
+// ---------------------------------------------------
+// Kruskal IST function
+// Returns: MST cost
+// If no MST exists (graph disconnected) return -1
+// ---------------------------------------------------
+ll kruskal_mst(int n, vector<Edge> &edges) {
+
+    sort(edges.begin(), edges.end(),
+         [](auto &a, auto &b) { return a.w < b.w; });
+
+    DSU dsu(n);
+    ll cost = 0;
+    int used = 0;
+
+    for (auto &e : edges) {
+        if (dsu.unite(e.u, e.v)) {
+            cost += e.w;
+            used++;
+        }
+    }
+
+    if (used != n - 1) return -1;  // no MST
+    return cost;
 }
 
-//Minimum deletion to make array sorted = n - LIS(a)
-//LIS on decreasing sequence -> multiply all in array by -1.
-
-//For 2d
-/*
-int solve_pairs(vector<pair<int,int>>& v){
-    sort(v.begin(), v.end(), [&](auto &a, auto &b){
-        if (a.first == b.first) return a.second < b.second;
-        return a.first < b.first;
-    });
-
-    vector<int> seq;
-    for(auto &p : v) seq.push_back(p.second);
-
-    return LIS(seq);
-}
-*/
-
-
-/*
- *
- *
- *
- */
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
@@ -48,12 +70,23 @@ int main() {
     cin >> T;
 
     while (T--) {
-        int n;
-        cin >> n;
-        vector<int> a(n);
-        for (int i = 0; i < n; i++) cin >> a[i];
 
-        cout << LIS(a) << "\n";
+        int n, m;
+        cin >> n >> m;
+
+        vector<Edge> edges(m);
+
+        for (int i = 0; i < m; i++) {
+            cin >> edges[i].u >> edges[i].v >> edges[i].w;
+        }
+
+        ll ans = kruskal_mst(n, edges);
+
+        if (ans == -1) cout << "NO MST\n";
+        else cout << ans << "\n";
+
         if (T) cout << "\n";
     }
+
+    return 0;
 }
